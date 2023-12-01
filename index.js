6ebda2175c062715a4fb438f5278356a684fd19e
Ala Amancio <alanalgm@gmail.com>
2023-12-01 16:30:32 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let heroi = Frida
+let pontuacaoDoHeroi = 1020
+let XP < 1000 = Ferro; XP > 1001 && 2000 = Bronze; XP > 2001 && 5000 = Prata; XP > 6001 && 7000 = Ouro; XP > 7001 && 8000 = Platina; XP > 8001 && 9000 = Ascendente; XP > 9001 && 10000 = Imortal; XP >= 10001  = Radiante
+ switch (XP) {
+    case "Ferro" :
+    console.log("O Herói Frida está no nível Ferro")
+    break;
+
+    case "Bronze" :
+    console.log("O Herói Frida está no nível Bronze")
+    break;
+
+    case "Prata" :
+    console.log("O Herói Frida está no nível Prata")
+    break;
+
+    case "Ouro" :
+    console.log("O Herói Frida está no nível Ouro")
+    break;
+
+    case "Platina" :
+    console.log("O Herói Frida está no nível Platina")
+    break;
+
+    case "Ascendente" :
+    console.log("O Herói Frida está no nível Ascendente")
+    break;
+
+    case "Imotal" :
+    console.log("O Herói Frida está no nível Imortal")
+    break;
+
+    case "Radiante" :
+    console.log("O Herói Frida está no nível Radiante")
+    break;
+
+    default:
+    console.log("Fim de Jogo")
+
+
+ }
+
+console.log
